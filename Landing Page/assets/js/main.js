793feff3fe1ec60e5ee792543a40fe4f0c817567
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-11-22 18:54:12 -0300

    validacao de campos concluida

--- Landing Page/assets/js/main.js
+++ Landing Page/assets/js/main.js
@@ -1,24 +1,67 @@
-const form = document.getElementById("form");
-const submitButton = document.getElementById("button-submit");
-
-const firstName = document.getElementById("first-name");
-const lastName = document.getElementById("last-name");
-const email = document.getElementById("email");
 const phone = document.getElementById("phone");
+const submitButton = document.getElementById("button-submit");
+const selectAnimals = document.getElementById("select-animals");
 
+const inputs = [
+  {
+    name: "first-name",
+    pattern: /\b([A-Za-z][a-z]+[ ]*)/,
+  },
+  {
+    name: "last-name",
+    pattern: /\b([A-Za-z][a-z]+[ ]*)/,
+  },
+  {
+    name: "email",
+    pattern: /\S+@\S+\.\S+/,
+  },
+  {
+    name: "phone",
+    pattern: /\(\d{2}\) \d{5}-\d{4}/,
+  }
+];
 
-phone.addEventListener('input', handleInput, false)
+phone.addEventListener("keypress", maskPhoneInput);
+submitButton.addEventListener("click", handleSubmit);
 
-function handleInput (e) {
-  e.target.value = phoneMask(e.target.value)
+const animais = ["Mel", "Nina", "Tommy", "Dom"];
+
+for (index in animais) {
+  const optionsNumber = selectAnimals.options.length;
+  selectAnimals.options[optionsNumber] = new Option(animais[index], index);
 }
 
-function phoneMask (phone) {
-  return phone.replace(/\D/g, '')
-    .replace(/^(\d)/, '($1')
-    .replace(/^(\(\d{2})(\d)/, '$1) $2')
-    .replace(/(\d{5})(\d{4})/, '$1-$2')
-    .replace(/(-\d{4})\d+?$/, '$1');
+function maskPhoneInput(e) {
+  const phoneNumber = e.target.value.replace(/[^\d]/g, "");
+
+  e.target.value = `(${phoneNumber.slice(0, 2)}) ${phoneNumber.slice(2,7)}-${phoneNumber.slice(7, 11)}`;
 }
 
+function handleSubmit(e) {
+  e.preventDefault();
+  validateInputs();
+}
 
+function validateInputs() {
+
+  inputs.forEach((input) => {
+
+    const inputElement = document.getElementById(input.name);
+    const errorMessage = inputElement.parentElement.querySelector(`.error-message`);
+
+    try {
+      if (!inputElement.value) {
+        throw (errorMessage.innerHTML = "Campo obrigatório");
+      } 
+      else if (!input.pattern.test(inputElement.value)) {
+        throw (errorMessage.innerHTML = "Formato inválido");
+      } 
+      else {
+        throw (errorMessage.innerHTML = "");
+      }
+    } 
+    catch (err) {
+      err;
+    }
+  });
+}
