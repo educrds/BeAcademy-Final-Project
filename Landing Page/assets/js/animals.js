16e9f52b3713b38d8bc78ea4ab61df732e2a7fcf
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-12-01 15:46:58 -0300

    adicionado efeito nos cards

--- Landing Page/assets/js/animals.js
+++ Landing Page/assets/js/animals.js
@@ -7,25 +7,34 @@ fetch(path)
   .catch((err) => console.log(err));
 
 function convertAnimalsToDiv(animal) {
-  const { name, type, photoUrl, description } = animal;
+  const { name, type, habitat, photoUrl, description } = animal;
 
-  loadAnimals(
-    `<div class="col-6 col-md-3">
+  loadAnimals(`
+    <div class="col-6 col-md-3">
       <div class="card">
-        <img src="${photoUrl}" alt="thumbnail animal ${name}"
-        />
+        <img src="${photoUrl}" />
         <div class="card-body">
-          <h5 class="card-title ">${name}</h5>
-          <p>
-            <i class="fa-solid fa-tag"></i> 
-            ${type}
-          </p>
-          <p>
-            <i class="fa-solid fa-circle-info"></i> ${description}
-          </p>
+          <h5 class="card-title">${name}</h5>
+          <div>
+            <span class="badge">${type}</span>
+            <span class="badge">${habitat}</span>
+            <a
+              role="btn"
+              data-bs-toggle="collapse"
+              href="#${name}"
+              aria-controls="${name}"
+              role="button"
+              aria-expanded="false"
+              ><i class="fa-solid fa-caret-down see-more"></i
+            ></a>
+            <div class="collapse multi-collapse card-text" id="${name}">
+              <div>${description}</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>`);
+    </div>
+  `);
 }
 
 function loadAnimals(animal) {
